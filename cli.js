d3b0942f9de6d22bd8d3fa2020ed55a7016ab683
amandaalmeida89 <amandaalmeida89@gmail.com>
2019-11-27 00:03:51 -0300

    listando links e texto

--- cli.js
+++ cli.js
@@ -1,4 +1,6 @@
 const { mdLinks } = require('./lib/index');
 
 const path = process.argv.slice(2).pop();
-mdLinks(path);
+mdLinks(path)
+  .then((response) => { console.log(response) })
+  .catch((error) => { console.log(error) });
