243ee15cbcfd37f0ccfa9fedd45462bade4fcab9
amandaalmeida89 <amandaalmeida89@gmail.com>
2019-11-27 21:09:39 -0300

    uso de fetch para validacao

--- cli.js
+++ cli.js
@@ -1,6 +1,8 @@
+#!/usr/bin/env node
+
 const { mdLinks } = require('./lib/index');
 
 const path = process.argv.slice(2).pop();
-mdLinks(path)
+mdLinks(path, { validate: true })
   .then((response) => { console.log(response) })
   .catch((error) => { console.log(error) });
